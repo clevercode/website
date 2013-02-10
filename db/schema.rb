6f058a9d8d945336ec52494ea1af15fb4e482883
Zachary Nicoll <tower@zdn.me>
2013-02-10 17:29:06 -0500

    Added gems and such

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,4 @@ ActiveRecord::Schema.define(:version => 20120821044740) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "pages", :force => true do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
 end
