96b909cdbd0ee419de5458d172cc9c0f241c9434
Zachary Nicoll <tower@zdn.me>
2013-04-03 15:21:36 -0400

    Added google tracking code, removed precompiled assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,18 @@
 //= require clevercode
 //= require_tree .
 
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-38730035-3']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(ga, s);
+})();
+
 $(document).ready(function(){
   // Ze Code.
 });
